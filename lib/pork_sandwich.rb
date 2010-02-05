86827690315e08f3d2afe031a1b87b727a206bca
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2010-02-05 02:27:58 -0500

    fixed another path problem

--- lib/pork_sandwich.rb
+++ lib/pork_sandwich.rb
@@ -7,12 +7,12 @@ require "#{File.dirname(__FILE__)}/pork_sandwich/auth"
 require "#{File.dirname(__FILE__)}/pork_sandwich/log"
 require "#{File.dirname(__FILE__)}/pork_sandwich/search"
 
-require "#{File.dirname(__FILE__)}/pork/table_classes/reaction"
-require "#{File.dirname(__FILE__)}/pork/table_classes/trend"
-require "#{File.dirname(__FILE__)}/pork/table_classes/tweet"
-require "#{File.dirname(__FILE__)}/pork/table_classes/tweet_reaction"
-require "#{File.dirname(__FILE__)}/pork/table_classes/twitter_account"
-require "#{File.dirname(__FILE__)}/pork/table_classes/twitter_relationship"
+require "#{File.dirname(__FILE__)}/pork_sandwich/table_classes/reaction"
+require "#{File.dirname(__FILE__)}/pork_sandwich/table_classes/trend"
+require "#{File.dirname(__FILE__)}/pork_sandwich/table_classes/tweet"
+require "#{File.dirname(__FILE__)}/pork_sandwich/table_classes/tweet_reaction"
+require "#{File.dirname(__FILE__)}/pork_sandwich/table_classes/twitter_account"
+require "#{File.dirname(__FILE__)}/pork_sandwich/table_classes/twitter_relationship"
 
 module Pork 
   #Object.send :undef_method, :id
