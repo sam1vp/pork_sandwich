3b9a5f878c649f91914a101bdfb5d07b2f0e3ee5
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2010-02-06 09:32:00 -0500

    called the auth method at a higher level so it only need be done once

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,7 @@ require "#{File.dirname(__FILE__)}/../lib/pork_sandwich"
 class Test::Unit::TestCase
   include RR::Adapters::TestUnit
   config = YAML::load(File.open("#{File.dirname(__FILE__)}/../config/pork_config.yml"))
-  $AUTH = Pork::Auth.new(config['config']['twitter_auth']['username'], config['config']['twitter_auth']['password'], :user_agent => 'web_ecology_project').auth
+  $AUTH = Pork::Auth.new(config['config']['twitter_auth']['username'], config['config']['twitter_auth']['password'], :user_agent => 'web_ecology_project')
 end
 
 
