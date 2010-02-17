d35697c02ac9211fa6520d8e3cf2dca1b935feb0
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2010-02-17 15:07:12 -0500

    made username and password readable for the Pork::Auth class

--- lib/pork_sandwich/auth.rb
+++ lib/pork_sandwich/auth.rb
@@ -1,7 +1,7 @@
 module Pork
   class Auth
     attr_accessor :auth
-  
+    attr_reader :username, :password
     def initialize(username, password, user_agent = '')
       @auth = Twitter::Base.new(Twitter::HTTPAuth.new(username, password, :user_agent => user_agent))
     end
